356cc0319600485a4ef5f7af114e0441cd963f5e
sivlek14 <kartigas323@gmail.com>
2015-06-29 19:08:44 +0000

    chat listo sin loging

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var app = require('express')(); var http = require('http').Server(app); app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
 });
-http.listen(3000, function(){
+
+io.on('connection', function(socket){
+   console.log('a user connected');
+  socket.on('chat_message', function(msg){
+    io.emit('chat_message', msg);
+  });
+});
+
+http.listen(process.env.PORT, process.env.IP, function(){
   console.log('listening on *:3000');
-});
+}).listen(process.env.PORT);
